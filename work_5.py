e60504ba6346b38298c506890b3752dd175d9c42
saodatjonqadamova5-max <saodatjonqadamova5@gmail.com>
2026-03-03 17:39:30 +0500

    work_4

---
+++ work_5.py
@@ -0,0 +1,42 @@
+#25
+# import math
+# x = int(input("x="))
+# y = 3 * math.pow(x, 6) - 6 * math.pow(x, 2) - 7
+# print(y)
+#26
+# import math
+# x=int(input("x="))
+# y = 4 * math.pow((x-3), 6) - 7 * math.pow((x-3), 3) + 2
+# print(y)
+#27
+# A=int(input("A="))
+# y1=A**2
+# y2=A**4
+# y3=A**8
+# print(y1,y2,y3)
+#28
+# A=int(input("A="))
+# y1=A**2
+# y2=A**3
+# y3=A**5
+# y4=A**10
+# y5=A**15
+# print(y1,y2,y3,y4,y5)
+#29
+# import math
+# a=int(input("a="))
+# rad = a * 2 * math.pi / 360
+# print(rad)
+#30
+# import math
+# a=int(input("a="))
+# grad = a * 360 / 2 * math.pi
+# print(grad)
+#31
+# Tf = int(input("Tf="))
+# Tc = (Tf - 32) * 5/9
+# print(Tc)
+#32
+# Tf = int(input("Tf="))
+# Tc = (Tf - 32) * 5/9
+# print(Tc)
